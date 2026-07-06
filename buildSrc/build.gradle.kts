3fd5a11ebd7d981ec18cbf13f1f9a6b4e199ba16
Leandro Carvalho <sj.leandro@gmail.com>
2026-07-06 14:19:58 -0300

    fix: downgrade detekt to 1.23.8 as 1.24.0 does not exist

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,5 +14,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.4.0")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.11.1")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:14.2.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.24.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.8")
 }
