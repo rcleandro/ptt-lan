670a0cd7604885137723f057522b93ca8c130d7b
Leandro Carvalho <sj.leandro@gmail.com>
2026-07-06 20:11:17 -0300

    chore: suppress unused warning for Ktor module function

--- serverApp/src/main/kotlin/com/pttlan/server/Application.kt
+++ serverApp/src/main/kotlin/com/pttlan/server/Application.kt
@@ -6,24 +6,19 @@ import io.ktor.network.tls.certificates.generateCertificate
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.application.port
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.engine.sslConnector
-import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
 import kotlinx.serialization.json.Json
 import java.io.File
-import java.io.FileInputStream
 import kotlin.time.Duration.Companion.seconds
-
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
-
 import java.net.InetAddress
 import io.ktor.server.netty.EngineMain
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
 
 @Suppress("TooGenericExceptionCaught", "MagicNumber")
 fun main(args: Array<String>) {
@@ -36,7 +31,7 @@ fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
-@Suppress("TooGenericExceptionCaught", "MagicNumber")
+@Suppress("TooGenericExceptionCaught", "MagicNumber", "unused")
 fun Application.module() {
     install(WebSockets) {
         pingPeriod = 20.seconds
@@ -77,9 +72,9 @@ fun Application.module() {
         }
     }.start()
 
-    install(org.koin.ktor.plugin.Koin) {
+    install(Koin) {
         modules(
-            org.koin.dsl.module {
+            module {
                 single {
                     ChannelRegistry()
                 }
