eeb72067b7f56e362d7b2727dfefb0fc3373c744
Leandro Carvalho <sj.leandro@gmail.com>
2026-07-07 14:30:13 -0300

    [PTT-007] style: fix ktlint issues in server test

--- serverApp/src/test/kotlin/com/pttlan/server/ServerIntegrationTest.kt
+++ serverApp/src/test/kotlin/com/pttlan/server/ServerIntegrationTest.kt
@@ -16,71 +16,74 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ServerIntegrationTest {
-
     @Test
-    fun testPttFloorControl() = testApplication {
-        application {
-            module()
-        }
+    fun testPttFloorControl() =
+        testApplication {
+            application {
+                module()
+            }
 
-        val client1 = createClient {
-            install(WebSockets)
-        }
-        val client2 = createClient {
-            install(WebSockets)
-        }
-
-        client1.webSocket("/ws?nickname=Client1") {
-            val session1 = this
-            
-            // Client1 joins
-            val join1 = ControlMessage.JoinChannel("channel-1", "Client1", "u1")
-            session1.send(Frame.Text(Json.encodeToString<ControlMessage>(join1)))
-            
-            client2.webSocket("/ws?nickname=Client2") {
-                val session2 = this
-                
-                // Client2 joins
-                val join2 = ControlMessage.JoinChannel("channel-1", "Client2", "u2")
-                session2.send(Frame.Text(Json.encodeToString<ControlMessage>(join2)))
-
-                // Client1 requests floor
-                val startSpeaking1 = ControlMessage.StartSpeaking("channel-1", "u1")
-                session1.send(Frame.Text(Json.encodeToString<ControlMessage>(startSpeaking1)))
-
-                // Read from session2 to get SpeakerChanged event confirming Client1 is speaking
-                // so we know they have the floor
-                // Actually wait, let's just make Client2 try to speak
-                val startSpeaking2 = ControlMessage.StartSpeaking("channel-1", "u2")
-                session2.send(Frame.Text(Json.encodeToString<ControlMessage>(startSpeaking2)))
-
-                // Client2 should receive FloorDenied
-                var receivedFloorDenied = false
-                
-                // We'll read incoming frames from Client2 until we get FloorDenied
-                try {
-                    val frame = session2.incoming.consumeAsFlow().filterIsInstance<Frame.Text>().first { frame ->
-                        val text = frame.readText()
-                        val msg = Json.decodeFromString<ControlMessage>(text)
-                        msg is ControlMessage.FloorDenied
-                    }
-                    val msg = Json.decodeFromString<ControlMessage>(frame.readText()) as ControlMessage.FloorDenied
-                    assertEquals("channel-1", msg.channelId)
-                    receivedFloorDenied = true
-                } catch (_: Exception) {
-                    // Closed or timeout
+            val client1 =
+                createClient {
+                    install(WebSockets)
+                }
+            val client2 =
+                createClient {
+                    install(WebSockets)
                 }
 
-                // Stop speaking for client 1 to clean up
-                val stop1 = ControlMessage.StopSpeaking("channel-1", "u1")
-                session1.send(Frame.Text(Json.encodeToString<ControlMessage>(stop1)))
-                
-                // Close sessions
-                session2.close()
-                session1.close()
+            client1.webSocket("/ws?nickname=Client1") {
+                val session1 = this
 
-                assertTrue(receivedFloorDenied, "Client2 should have received FloorDenied message")
+                // Client1 joins
+                val join1 = ControlMessage.JoinChannel("channel-1", "Client1", "u1")
+                session1.send(Frame.Text(Json.encodeToString<ControlMessage>(join1)))
+
+                client2.webSocket("/ws?nickname=Client2") {
+                    val session2 = this
+
+                    // Client2 joins
+                    val join2 = ControlMessage.JoinChannel("channel-1", "Client2", "u2")
+                    session2.send(Frame.Text(Json.encodeToString<ControlMessage>(join2)))
+
+                    // Client1 requests floor
+                    val startSpeaking1 = ControlMessage.StartSpeaking("channel-1", "u1")
+                    session1.send(Frame.Text(Json.encodeToString<ControlMessage>(startSpeaking1)))
+
+                    // Read from session2 to get SpeakerChanged event confirming Client1 is speaking
+                    // so we know they have the floor
+                    // Actually wait, let's just make Client2 try to speak
+                    val startSpeaking2 = ControlMessage.StartSpeaking("channel-1", "u2")
+                    session2.send(Frame.Text(Json.encodeToString<ControlMessage>(startSpeaking2)))
+
+                    // Client2 should receive FloorDenied
+                    var receivedFloorDenied = false
+
+                    // We'll read incoming frames from Client2 until we get FloorDenied
+                    try {
+                        val frame =
+                            session2.incoming.consumeAsFlow().filterIsInstance<Frame.Text>().first { frame ->
+                                val text = frame.readText()
+                                val msg = Json.decodeFromString<ControlMessage>(text)
+                                msg is ControlMessage.FloorDenied
+                            }
+                        val msg = Json.decodeFromString<ControlMessage>(frame.readText()) as ControlMessage.FloorDenied
+                        assertEquals("channel-1", msg.channelId)
+                        receivedFloorDenied = true
+                    } catch (_: Exception) {
+                        // Closed or timeout
+                    }
+
+                    // Stop speaking for client 1 to clean up
+                    val stop1 = ControlMessage.StopSpeaking("channel-1", "u1")
+                    session1.send(Frame.Text(Json.encodeToString<ControlMessage>(stop1)))
+
+                    // Close sessions
+                    session2.close()
+                    session1.close()
+
+                    assertTrue(receivedFloorDenied, "Client2 should have received FloorDenied message")
+                }
             }
         }
-    }
 }
