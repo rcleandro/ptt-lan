c20dfe3e1224bd803fc49ccd72b05c29acaaceb3
Leandro Carvalho <sj.leandro@gmail.com>
2026-07-07 14:38:16 -0300

    [PTT-007] style: fix ktlint issues in shared MainViewController

--- shared/src/iosMain/kotlin/com/pttlan/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/pttlan/shared/MainViewController.kt
@@ -3,16 +3,18 @@ package com.pttlan.shared
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.pttlan.core.designsystem.theme.PttTheme
 import com.pttlan.core.navigation.RootComponent
 import com.pttlan.core.navigation.RootScreen
-import com.pttlan.core.designsystem.theme.PttTheme
 import platform.UIKit.UIViewController
 
+@Suppress("FunctionName")
 fun MainViewController(): UIViewController {
     val lifecycle = LifecycleRegistry()
-    val rootComponent = RootComponent(
-        componentContext = DefaultComponentContext(lifecycle = lifecycle)
-    )
+    val rootComponent =
+        RootComponent(
+            componentContext = DefaultComponentContext(lifecycle = lifecycle),
+        )
 
     return ComposeUIViewController {
         PttTheme {
