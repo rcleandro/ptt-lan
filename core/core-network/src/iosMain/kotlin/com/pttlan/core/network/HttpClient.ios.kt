cb2e64313a9475e60692a46e1149f261cac0d088
Leandro Carvalho <sj.leandro@gmail.com>
2026-07-08 03:05:24 -0300

    fix(ios): add BetaInteropApi opt-in for cinterop convert

--- core/core-network/src/iosMain/kotlin/com/pttlan/core/network/HttpClient.ios.kt
+++ core/core-network/src/iosMain/kotlin/com/pttlan/core/network/HttpClient.ios.kt
@@ -2,6 +2,7 @@ package com.pttlan.core.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.convert
 import platform.Foundation.NSURLCredential
@@ -10,7 +11,7 @@ import platform.Foundation.NSURLSessionAuthChallengeUseCredential
 import platform.Foundation.create
 import platform.Foundation.serverTrust
 
-@OptIn(ExperimentalForeignApi::class)
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
 actual fun createPlatformHttpClient(): HttpClient =
     HttpClient(Darwin) {
         engine {
